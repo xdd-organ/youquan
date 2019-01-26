c62d6bbde2956ff18f01e2902f5162596edefcf8
tyetao <765068439@qq.com>
2019-01-26 22:11:10 +0800

    修改logo图

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="header">
-        <span class="group_name">优荃电子</span>
-        <!--<div class="search"><input placeholder="请输入关键字" clearable/></div>-->
-        <div class="search">服务热线：13802570139
-            <Input search placeholder="请输入关键字"/>
+        <div class="header_ logo"></div>
+        <div class="header_ header_right">
+            <div class="search">服务热线：13802570139
+                <Input search placeholder="请输入关键字"/>
+            </div>
         </div>
+        <!--<span class="group_name">优荃电子</span>-->
+        <!--<div class="search"><input placeholder="请输入关键字" clearable/></div>-->
+
     </div>
 </template>
 
@@ -22,19 +26,30 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="less">
     .header {
         /*background: aqua;*/
         height: 100px;
         margin-top: 10px;
+        .header_ {
+            float: left;
+            height: 100px;
+        }
+        .logo {
+            background: url("../../assets/imgs/logo.png") no-repeat;
+            width: 400px;
+        }
+        .header_right {
+        }
     }
     .group_name {
         font-size: 30px;
         font-weight: bold;
     }
     .search {
+        margin-top: 50px;
+        margin-left: 175%;
         width: 200px;
-        height: 30px;
-        margin-left: 79%;
+        height: 100px;
     }
 </style>
